300c34af72c65931f56b2ef2eabc427c7f0ddc63
高岸知広 <t.takagishi.cingroup2012@gmail.com>
2023-11-29 19:16:59 +0900

    add 静的コンテンツのルーティング

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const PORT = process.env.PORT;
+
+const path = require("path");
+
 const express = require("express");
 const app = express();
 
+// expressの設定
 app.set("view engine", "ejs");
 
+// 静的コンテンツのルーティング
+app.use("/public", express.static(path.join(__dirname, "/public")));
+
+//動的コンテンツのルーティング
 app.use("/", require("./routes/index.js"));
 
+// WEBアプリの実行
 app.listen(PORT, () => {
   console.log(`Application listening at ${PORT}`);
 });
