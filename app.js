99cee15fb1f258fd505ef26afc35ebda8798d5d1
高岸知広 <t.takagishi.cingroup2012@gmail.com>
2023-11-29 20:59:17 +0900

    fix metaを共通化

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.set("view engine", "ejs");
 app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "/public")));
 
+
 //動的コンテンツのルーティング
 app.use("/", require("./routes/index.js"));
 
