586fa68bd198d23eaadc9a55b62365b218a3386b
高岸知広 <t.takagishi.cingroup2012@gmail.com>
2023-11-29 20:42:12 +0900

    add:serve-favicon@^2.5.0

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const PORT = process.env.PORT;
-
+const PORT = process.env.PORT || 3000;
 const path = require("path");
-
 const express = require("express");
+const favicon = require("serve-favicon");
 const app = express();
+
 
 // expressの設定
 app.set("view engine", "ejs");
 
 // 静的コンテンツのルーティング
+app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "/public")));
 
 //動的コンテンツのルーティング
@@ -16,6 +17,6 @@ app.use("/", require("./routes/index.js"));
 
 // WEBアプリの実行
 app.listen(PORT, () => {
-  console.log(`Application listening at ${PORT}`);
+  console.log(`Application listening at :${PORT}`);
 });
 
